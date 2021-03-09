98eeaf4d8ae6d5a67901a613eec5bee234bd667f
binay shah <binayshah@binays-MacBook-Pro.local>
2021-03-09 13:49:04 +0545

    initial commit pricing microservice

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into price (id, currency, price, vehicle_id) values(1, 'USD', 5000.00, 1);
+insert into price (id, currency, price, vehicle_id)  values(2, 'USD', 5000.00, 2);
+insert into price(id, currency, price, vehicle_id)  values(3, 'USD', 5000.00, 3);
+insert into price(id, currency, price, vehicle_id)  values(4, 'USD', 5000.00, 4);
+insert into price(id, currency, price, vehicle_id)  values(5, 'USD', 5000.00, 5);
+insert into price(id, currency, price, vehicle_id) values(6, 'USD', 5000.00, 6);
